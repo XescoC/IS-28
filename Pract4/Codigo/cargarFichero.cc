eb67c96a0d057d407365e8cf965caffccc0d994e
Andrés Ruiz Ventura <i72ruvea@uco.es>
2018-12-15 19:56:08 +0100

    Funciones cargar y guardar fichero

---
+++ Pract4/Codigo/cargarFichero.cc
@@ -0,0 +1,52 @@
+#include "alumnos.h"
+#include "agenda.h"
+
+//Función que se encarga de cargar la lista de alumnos en un fichero binario.
+void Agenda::cargarFichero()
+{
+	ifstream fichEntrada;
+	fichEntrada.open("alumnos.bin");
+	if (fichEntrada)
+	{
+		list <Alumno>::iterator pos;
+		Alumno alum=NULL;
+		vector_.clear();
+		string linea;
+		bool lider;
+		while(getline(fichEntrada, linea, ',')){
+			alum.setDNI(linea);
+			getline(fichEntrada,linea,',');
+			alum.setNombre(linea);
+			getline(fichEntrada,linea,',');
+			alum.setApellido1(linea);
+			getline(fichEntrada,linea,',');
+			alum.setApellido2(linea);
+			getline(fichEntrada,linea,',');
+			alum.setEmail(linea);
+			getline(fichEntrada,linea,',');
+			alum.setDireccion(linea);
+			getline(fichEntrada,linea,',');
+			alum.setCurso(atoi(linea.c_str()));
+			getline(fichEntrada,linea,',');
+			alum.setFecha(linea);
+			getline(fichEntrada,linea,',');
+			alum.setGrupo(atoi(linea.c_str()));
+			getline(fichEntrada,linea,'\n');
+			if (linea=="true")
+			{
+				lider=true;
+			}
+			else
+			{
+				lider=false;
+			}
+			alum.setLider(lider);
+
+			vector_.push_back(alum);
+		}
+	}
+	else
+	{
+		cout << "Error al abrir el fichero binario." << endl;
+	}
+}
---
+++ Pract4/Codigo/guardarFichero.cc
@@ -0,0 +1,42 @@
+#include "alumnos.h"
+#include "agenda.h"
+
+//Función que se encarga de guardar la lista de alumnos en un fichero binario.
+void Agenda::guardarFichero()
+{
+	ofstream fichSalida;
+	fichSalida.open("alumnos.bin");
+	if (fichSalida)
+	{
+		list <Alumno>::iterator pos;
+		Alumno alum=NULL;
+		for(pos = vector_.begin(); pos != vector_.end(); pos++)
+		{
+			alum=(*pos);
+
+			//Escribe en un fichero .md la información de cada alumno
+			//DNI,nombre,apellido1,apellido2,email,direccion,curso,fecha,grupo\n
+			fichSalida <<alum.getDNI()<<",";
+			fichSalida << alum.getNombre()<<",";
+			fichSalida << alum.getApellido1()<<",";
+			fichSalida <<alum.getApellido2()<<",";
+			fichSalida << alum.getEmail()<<",";
+			fichSalida << alum.getDireccion()<<",";
+			fichSalida << alum.getCurso()<<",";
+			fichSalida << alum.getFecha()<<",";
+			fichSalida << alum.getGrupo()<<",";
+			if (alum.getLider()==true)
+			{
+				fichSalida << "true" << endl;
+			}
+			else
+			{
+				fichSalida << "false" << endl;
+			}
+		}
+	}
+	else
+	{
+		cout << "Error al crear el fichero markdown." << endl;
+	}
+}
