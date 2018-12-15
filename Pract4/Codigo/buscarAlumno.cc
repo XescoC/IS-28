e6047f9a813f42d08d43ec297b555d04121dffb4
XescoC <p32cubaf@uco.es>
2018-12-15 07:16:49 -0800

    Corrección de código 4

--- Pract4/Codigo/buscarAlumno.cc
+++ Pract4/Codigo/buscarAlumno.cc
@@ -2,11 +2,9 @@
 #include "alumno.h"
 //Función que se encarga de buscar el alumno por DNI o por su primer apellido
 //Devuelve el objeto de la clase Alumno en la que se encuentra el alumno o NULL si no encuentra ninguno
-int Agenda::buscarAlumno()
+Alumno Agenda::buscarAlumno()
 {
-	Alumno alum=NULL;
-	do{
-		//Menu para elegir la manera en la que se desea buscar al alumno
+	Alumno alum;	
 		do{
 			opcion=menuBuscarAlumno();
 			switch(opcion){
@@ -23,41 +21,37 @@ int Agenda::buscarAlumno()
 			}
 		}while(opcion!=0);
 
-		if (alum==	NULL)
-		{
-			cout << "No se encontró ningun alumno con los datos introducidos." << endl;
-		}
-
-	}while(alum!=NULL);
-	//Bucle do-while que se ejecuta cada vez que no se encuentre ningun alumno
+		
 	
-	return i;
+	return alum;
 }
-int Agenda::buscarAlumnoDNI(string DNI)
+Alumno Agenda::buscarAlumnoDNI(string DNI)
 {
 	list <Alumno>::iterator pos;
+	Alumno alum=NULL;
 	pos=vector_.begin();
 	for(pos = vector_.begin(); pos != vector_.end(); pos++)
 	{
 		if (pos->getDNI()==DNI)
 		{
-			return pos;
+			alum=(*pos);
+			return alum;
 		}
 	}
-	return -1;
+	return alum;
 }	
 
-int Agenda::buscarAlumnoApellido(string apellido)
+Alumno Agenda::buscarAlumnoApellido(string apellido)
 {
 	int contador=0;
 	list <Alumno>::iterator pos;
-	int i=-1;
+	Alumno alum=NULL;
 	for(pos = vector_.begin(); pos != vector_.end(); pos++)
 	{
 		if (pos->getApellido1()==apellido)
 		{
 			contador ++;
-			i=pos;
+			alum=(*pos);
 		}
 	}
 	//En caso de que encuentre 2 o mas alumnos con el mismo apellido ejecutara la funcion de buscar por DNI
@@ -66,21 +60,21 @@ int Agenda::buscarAlumnoApellido(string apellido)
 		string DNI;
 		cout << "Existe más un alumno con este apellido, introduzca su DNI:" <<endl;
 		cin >> DNI;
-		i=buscarAlumnoDNI(DNI);
-		return i;
+		alum=buscarAlumnoDNI(DNI);
+		return alum;
 	}
-	else
+	else 
 	{
-		return i;
+		return alum;
 	}
 }
 int Agenda::menuBuscarAlumno()
 {
 	int opcion;
 	cout << "Buscar alumno:" << endl 
-	<< "Opción 0: Cancelar búsqueda" << endl
-	<< "Opción 1: Por DNI" << endl 
-	<< "Opción 2: Por apellido" << endl;
+	<< "	Opción 0: Cancelar búsqueda" << endl
+	<< "	Opción 1: Por DNI" << endl 
+	<< "	Opción 2: Por apellido" << endl;
 	cin >> opcion;
 	return opcion;
 }
