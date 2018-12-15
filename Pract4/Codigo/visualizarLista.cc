266c00f3438d8e5672125ab81e25f9f9333d7420
XescoC <p32cubaf@uco.es>
2018-12-15 09:56:48 -0800

    Corrección de código 6

--- Pract4/Codigo/visualizarLista.cc
+++ Pract4/Codigo/visualizarLista.cc
@@ -4,6 +4,7 @@
 //Función que se encarga de imprimir los datos de todos los alumnos y volcarlo a un fichero markdown.
 void Agenda::visualizarLista()
 {
+	string lider;
 	ofstream fichSalida;
 	fichSalida.open("lista.md");
 	if (fichSalida)
@@ -15,25 +16,37 @@ void Agenda::visualizarLista()
 		{
 			alum=(*pos);
 			//Escribe por pantalla la información de cada alumno
-			cout<<"DNI:"<<alum.getDNI()<<endl;
-			cout<<"Nombre:"<<alum.getNombre()<<endl;
-			cout<<"Apellidos:"<<alum.getApellido1()<<" "<<alum.getApellido2()<<endl;
-			cout<<"Email:"<<alum.getEmail()<<endl;
-			cout<<"Direccion:"<<alum.getDireccion()<<endl;
+			cout<<"DNI: "<<alum.getDNI()<<endl;
+			cout<<"Nombre: "<<alum.getNombre()<<endl;
+			cout<<"Apellidos: "<<alum.getApellido1()<<" "<<alum.getApellido2()<<endl;
+			cout<<"Email: "<<alum.getEmail()<<endl;
+			cout<<"Direccion: "<<alum.getDireccion()<<endl;
 			cout<<"Curso:"<<alum.getCurso()<<endl;
-			cout<<"Fecha de nacimiento:"<<alum.getFecha()<<endl;
-			cout<<"Grupo:"<<alum.getGrupo()<<endl;
+			cout<<"Fecha de nacimiento: "<<alum.getFecha()<<endl;
+			cout<<"Grupo: "<<alum.getGrupo()<<endl;
+			if (alum.getLider()==true)
+			{
+				lider="Sí";
+			}
+			else 
+			{
+				lider="No";
+			}
+			cout<<"Lider:"<<lider<<endl;
+
+
 			cout << "-------------"<<endl;
 			
 			//Escribe en un fichero .md la información de cada alumno
-			fichSalida << "DNI:"<<alum.getDNI()<<endl<<endl;
-			fichSalida << "Nombre:"<<alum.getNombre()<<endl;
-			fichSalida << "Apellidos:"<<alum.getApellido1()<<" "<<alum.getApellido2()<<endl<<endl;
-			fichSalida << "Email:"<<alum.getEmail()<<endl<<endl;
-			fichSalida << "Direccion:"<<alum.getDireccion()<<endl<<endl;
-			fichSalida << "Curso:"<<alum.getCurso()<<endl<<endl;
-			fichSalida << "Fecha de nacimiento:"<<alum.getFecha()<<endl;
-			fichSalida << "Grupo:"<<alum.getGrupo()<<endl<<endl;
+			fichSalida << "DNI: "<<alum.getDNI()<<endl<<endl;
+			fichSalida << "Nombre: "<<alum.getNombre()<<endl;
+			fichSalida << "Apellidos: "<<alum.getApellido1()<<" "<<alum.getApellido2()<<endl<<endl;
+			fichSalida << "Email: "<<alum.getEmail()<<endl<<endl;
+			fichSalida << "Direccion: "<<alum.getDireccion()<<endl<<endl;
+			fichSalida << "Curso: "<<alum.getCurso()<<endl<<endl;
+			fichSalida << "Fecha de nacimiento: "<<alum.getFecha()<<endl;
+			fichSalida << "Grupo: "<<alum.getGrupo()<<endl<<endl;
+			fichSalida << "Lider: "<<lider<<endl<<endl;
 			fichSalida <<"-----------"<<endl<<endl;
 		}
 	}
